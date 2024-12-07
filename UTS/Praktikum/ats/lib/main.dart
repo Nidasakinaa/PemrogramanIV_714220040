6232b207034e6ff6e7bd9a9501d3f8346066ab6e
nida sakinah aulia <nidasakinaaulia@gmail.com>
2024-12-07 16:21:39 +0700

    UTS

---
+++ UTS/Praktikum/ats/lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:ats/bottom_navbar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: false,
+      ),
+      home: DynamicBottomNavbar(),
+    );
+  }
+}
+
+class MyInput extends StatefulWidget {
+  const MyInput({super.key});
+
+  @override
+  State<MyInput> createState() => _MyInputState();
+}
+
+class _MyInputState extends State<MyInput> {
+  TextEditingController _controller = TextEditingController();
+  bool lightOn = false;
+  String? language;
+  bool agree = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Input Widget'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                hintText: 'Write your name here...',
+                labelText: 'Your Name',
+              ),
+            ),
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                hintText: 'Write your NPM here...',
+                labelText: 'Your NPM',
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              child: const Text('Submit'),
+              onPressed: (){
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: Text('Hello, ${_controller.text}'),
+                    );
+                  });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void showSnackBar() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('$language selected'),
+        duration: Duration(seconds: 1),
+      ),
+    );
+  }
+
+  @override
+  void_dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
